4e02ea441a4c4752f9f8368f70aa037b62d6aca5
Minjae Kang <rkdalswo1021@naver.com>
2023-01-21 20:46:07 +0900

    chore: Repository 삭제

--- src/repositories/DailyPlanRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { DailyPlan } from '../entities/Plan';
-
-@EntityRepository(DailyPlan)
-export class DailyPlanRepository extends Repository<DailyPlan> {}
--- src/repositories/RoutineRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { RoutineRoad } from './../entities/RoutineRoad';
-
-@EntityRepository(RoutineRoad)
-export class RoutineRepository extends Repository<RoutineRoad> {}
