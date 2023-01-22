4f106ed0089041dc9c205b3a1cdf5b7ecf14d875
Minjae Kang <rkdalswo1021@naver.com>
2023-01-22 19:08:34 +0900

    feat: 계획 블록 삭제 구현

--- src/controllers/PlanController.ts
+++ src/controllers/PlanController.ts
@@ -10,6 +10,7 @@ import {
   QueryParams,
   UseBefore,
   UseAfter,
+  Delete,
 } from 'routing-controllers';
 import errorValidator from '../middleware/errorValidator';
 import { IsString } from 'class-validator';
@@ -23,7 +24,7 @@ import { success, fail } from '../modules/util';
 class GetTypeAndDateQuery {
   @IsString()
   type!: string;
-  planDate!: string;
+  date!: string;
 }
 
 @JsonController('/plan')
@@ -47,11 +48,50 @@ export class DailyPlanController {
       const data = await this.dailyPlanService.getPlans(
         +userId,
         query.type,
-        query.planDate,
+        query.date,
       );
       return res
         .status(statusCode.OK)
         .send(success(statusCode.OK, message.READ_PLAN_SUCCESS, data));
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR),
+        );
+    }
+  }
+
+  @HttpCode(200)
+  @Delete('/:userId/:planId')
+  @OpenAPI({
+    summary: '계획 블록 삭제',
+    description: '일간 계획, 우회할 계획, 루틴로드 계획블록 삭제',
+    statusCode: '200',
+  })
+  public async deletePlans(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param('userId') userId: string,
+    @Param('planId') planId: string,
+    @QueryParams() query: GetTypeAndDateQuery,
+  ) {
+    try {
+      const data = await this.dailyPlanService.deletePlans(
+        +userId,
+        +planId,
+        query.type,
+        query.date,
+      );
+      if (!data) {
+        return res
+          .status(statusCode.BAD_REQUEST)
+          .send(fail(statusCode.BAD_REQUEST, message.DELETE_PLAN_FAIL));
+      }
+      return res
+        .status(statusCode.OK)
+        .send(success(statusCode.OK, message.DELETE_PLAN_SUCCESS));
     } catch (error) {
       console.log(error);
       return res
