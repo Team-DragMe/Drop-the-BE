e67e1345930454da10452f1fcd83ed20e6c58299
Chedda98 <twa05157@gmail.com>
2023-01-23 02:35:44 +0900

    feat: 데일리노트 조회 기능 구현

--- src/services/DailyNoteService.ts
+++ src/services/DailyNoteService.ts
@@ -7,4 +7,21 @@ export class DailyNoteService {
   constructor(
     @InjectRepository() private dailyNoteRepository: DailyNoteRepository,
   ) {}
+
+  public async getDailyNote(userId: number, createdAt: string) {
+    try {
+      const userDailyNote = await this.dailyNoteRepository.find({
+        where: {
+          user_id: userId,
+          createdAt,
+        },
+      });
+
+      if (!userDailyNote) {
+        return null; // (확인해보기)널 말고 메시지 바로 보내주는게 클라가 편하려나?
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 }
