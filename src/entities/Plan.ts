0feb4a83e944eec9e201c89e431e727c1c87a192
Minjae Kang <rkdalswo1021@naver.com>
2023-02-04 14:03:28 +0900

    chore: isCompleted 컬럼 추가
    계획 블록 완료/미완료를 판단하기 위해서 컬럼을 추가하였습니다.

--- src/entities/Plan.ts
+++ src/entities/Plan.ts
@@ -12,7 +12,7 @@ export class Plan {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ name: 'plan_date', nullable: false })
+  @Column({ name: 'plan_date', nullable: true })
   planDate?: Date;
 
   @Column({ name: 'plan_name', length: 100, nullable: false })
@@ -26,6 +26,9 @@ export class Plan {
 
   @Column('int', { name: 'fulfill_time', array: true, nullable: true })
   fulfillTime?: number[];
+
+  @Column('boolean', { name: 'is_completed', nullable: false, default: false })
+  isCompleted?: boolean;
 
   @ManyToOne(() => User, (user) => user.dailyplans)
   user?: User;
