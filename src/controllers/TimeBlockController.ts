705458a14b0bca7ddc18a61c7ae03433b21a5626
Minjae Kang <rkdalswo1021@naver.com>
2023-03-25 18:29:38 +0900

    chore: 타임블록 설정 유효성 검사 추가

--- src/controllers/TimeBlockController.ts
+++ src/controllers/TimeBlockController.ts
@@ -21,6 +21,7 @@ import {
 } from '../middleware/errorValidator';
 import { generalErrorHandler } from './../middleware/errorHandler';
 import { TimeBlockDto } from '../dtos/TimeBlockDto';
+import { validate } from 'class-validator';
 
 @JsonController('/timeblock')
 export class TimeBlockController {
@@ -38,10 +39,22 @@ export class TimeBlockController {
   public async setData(
     @Res() res: Response,
     @Param('planId') planId: number,
-    @Body({ validate: true }) timeBlockDto: TimeBlockDto,
+    @Body() body: { isPlan: boolean; start: number; end: number },
   ): Promise<Response> {
-    const { isPlan, start, end } = timeBlockDto;
+    const timeBlockDto = new TimeBlockDto();
+    timeBlockDto.isPlan = body.isPlan;
+    timeBlockDto.start = body.start;
+    timeBlockDto.end = body.end;
+
+    const errors = await validate(timeBlockDto);
+    if (errors.length != 0) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+    }
+
     try {
+      const { isPlan, start, end } = timeBlockDto;
       const userId = res.locals.JwtPayload;
       await this.timeBlockService.setTimeBlock(
         userId,
