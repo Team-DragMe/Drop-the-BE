0c90dbeb26edda31c31492526a7f996639a5708b
Minjae Kang <rkdalswo1021@naver.com>
2023-03-28 15:19:05 +0900

    chore: DailyNoteDto Matches 데코레이터 추가

--- src/dtos/DailyNoteDto.ts
+++ src/dtos/DailyNoteDto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateDailyNoteDto {
   @IsNotEmpty()
   @IsString()
+  @Matches(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/)
   planDate!: string;
 
   @IsString()
